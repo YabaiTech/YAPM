c9a22b1ef423e7fc90cc76c1a65ebc0951902705
Twaha Rahman <mahi6703890@gmail.com>
2025-06-11 16:59:07 +0600

    Add the dependency required for MySQL database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    implementation("com.mysql:mysql-connector-j:9.3.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
