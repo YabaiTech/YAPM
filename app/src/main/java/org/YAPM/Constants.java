4a62e990d6403d31a808990f1fa71802c97263e2
Anindya-ctrl <and_@tutanota.com>
2025-06-11 08:44:26 +0600

    added necessary constants

---
+++ app/src/main/java/org/YAPM/Constants.java
@@ -0,0 +1,10 @@
+package org.YAPM;
+
+public class Constants {
+  public static final String ENCRYPTION_ALGO = "AES/CBC/PKCS5Padding";
+  public static final String KEY_DERIVATION_FUNCTION = "PBKDF2WithHmacSHA256";
+  public static final int ITERATIONS = 65536;
+  public static final int KEY_LENGTH = 256;
+  public static final int IV_LENGTH = 16;
+  public static final int SALT_LENGTH = 16;
+}
