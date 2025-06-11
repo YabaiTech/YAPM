b0f4500e9bd949e68ea14c194a2f02619701f43d
M-H-MARUF-1903 <md.muktadirul.haque.maruf@gmail.com>
2025-06-11 14:01:27 +0600

    Add Login UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,13 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+
+    // The following are
+    // groupId:     com.formdev
+    // artifactId:  flatlaf-intellij-themes
+    // version:     3.6 (the current latest version)
+    implementation("com.formdev:flatlaf:3.6")
+    implementation("com.formdev:flatlaf-intellij-themes:3.6")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
--- app/src/main/java/org/YAPM/App.java
+++ app/src/main/java/org/YAPM/App.java
@@ -1,11 +1,22 @@
 package org.YAPM;
 
+import javax.swing.*;
+        import com.formdev.flatlaf.intellijthemes.FlatNordIJTheme;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        try {
+            FlatNordIJTheme.setup(); // Apply the Nord theme
+        } catch (Exception e) {
+            System.err.println("Failed to apply theme.");
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            new LoginUI().setVisible(true); // Show login UI by default
+        });
     }
 }
