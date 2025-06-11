9dee910cf485a34bcbba33dbc4a9ef1aa65d9779
M-H-MARUF-1903 <md.muktadirul.haque.maruf@gmail.com>
2025-06-11 16:05:16 +0600

    Adds Login Panel

--- app/src/main/java/org/YAPM/MainUI.java
+++ app/src/main/java/org/YAPM/MainUI.java
@@ -10,19 +10,19 @@ public class MainUI extends JFrame {
     public MainUI() {
         setTitle("YAPM");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setSize(600, 700);
         setLocationRelativeTo(null);
 
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
 
         LoginPanel loginPanel = new LoginPanel(this);
-        RegisterPanel registerPanel = new RegisterPanel(this);
-        HomePanel homePanel = new HomePanel();
+//        RegisterPanel registerPanel = new RegisterPanel(this);
+//        HomePanel homePanel = new HomePanel();
 
         cardPanel.add(loginPanel, "login");
-        cardPanel.add(registerPanel, "register");
-        cardPanel.add(homePanel, "home");
+//        cardPanel.add(registerPanel, "register");
+//        cardPanel.add(homePanel, "home");
 
         add(cardPanel);
         cardLayout.show(cardPanel, "login"); // show login by default
