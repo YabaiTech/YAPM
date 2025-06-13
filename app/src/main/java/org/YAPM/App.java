87f21cfc74d454c68498956bda9d8233be01edae
Twaha Rahman <mahi6703890@gmail.com>
2025-06-13 12:34:58 +0600

    Remove unnecessary auto-generated things

--- app/src/main/java/org/YAPM/App.java
+++ app/src/main/java/org/YAPM/App.java
@@ -1,23 +1,18 @@
-
 package org.YAPM;
 
 import com.formdev.flatlaf.intellijthemes.FlatNordIJTheme;
 import javax.swing.*;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
+  public static void main(String[] args) {
+    try {
+      FlatNordIJTheme.setup();
+    } catch (Exception e) {
+      System.err.println("Failed to apply theme");
     }
 
-    public static void main(String[] args) {
-        try {
-            FlatNordIJTheme.setup();
-        } catch (Exception e) {
-            System.err.println("Failed to apply theme");
-        }
-
-        SwingUtilities.invokeLater(() -> {
-            new MainUI().setVisible(true);
-        });
-    }
+    SwingUtilities.invokeLater(() -> {
+      new MainUI().setVisible(true);
+    });
+  }
 }
