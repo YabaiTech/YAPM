00d564bb106ec46ed9c4e5be3c3c9d423d280884
Anindya-ctrl <and_@tutanota.com>
2025-06-13 13:11:03 +0600

    included id in Entry class

--- app/src/main/java/org/vault/Entry.java
+++ app/src/main/java/org/vault/Entry.java
@@ -1,14 +1,20 @@
 package org.vault;
 
 public class Entry {
+  private final int id;
   private final String url;
   private final String username;
   private final String passwd;
 
-  Entry(String url, String username, String passwd) {
+  Entry(int id, String url, String username, String passwd) {
+    this.id = id;
     this.url = url;
     this.username = username;
     this.passwd = passwd;
+  }
+
+  public int getID() {
+    return this.id;
   }
 
   public String getURL() {
