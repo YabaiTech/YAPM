7cbdb3c825bda3781007267010bd8b4b4e1b352d
Twaha Rahman <mahi6703890@gmail.com>
2025-06-13 10:59:06 +0600

    Close the PreparedStatements when done

--- app/src/main/java/org/backend/DBOperations.java
+++ app/src/main/java/org/backend/DBOperations.java
@@ -15,82 +15,85 @@ public class DBOperations {
     String query = "INSERT INTO `" + EnvVars.MASTER_USER_TABLE
         + "` (username, email, hashed_password, salt, pwd_db_path, last_logged_in) VALUES (?,?,?,?,?,?)";
 
-    PreparedStatement ps = con.prepareStatement(query);
+    try (PreparedStatement ps = con.prepareStatement(query)) {
+      ps.setString(1, username);
+      ps.setString(2, email);
+      ps.setString(3, hashedPassword);
+      ps.setString(4, saltB64);
+      ps.setString(5, dbFilePath);
+      ps.setLong(6, lastUnixTimestamp);
 
-    ps.setString(1, username);
-    ps.setString(2, email);
-    ps.setString(3, hashedPassword);
-    ps.setString(4, saltB64);
-    ps.setString(5, dbFilePath);
-    ps.setLong(6, lastUnixTimestamp);
+      int opStat = ps.executeUpdate();
 
-    int opStat = ps.executeUpdate();
+      if (opStat != 1) {
+        return new BackendError(BackendError.ErrorTypes.DbTransactionError,
+            "[DBOperations.addUser] Failed to add user");
+      }
 
-    if (opStat != 1) {
-      return new BackendError(BackendError.ErrorTypes.DbTransactionError, "[DBOperations.addUser] Failed to add user");
+      return null;
     }
-
-    return null;
   }
 
   public UserInfo getUserInfo(String username) throws SQLException {
     String query = "SELECT * FROM `" + EnvVars.MASTER_USER_TABLE + "` WHERE `username`=?";
 
-    PreparedStatement ps = con.prepareStatement(query);
+    try (PreparedStatement ps = con.prepareStatement(query)) {
+      ps.setString(1, String.valueOf(username));
+      ResultSet res = ps.executeQuery();
 
-    ps.setString(1, String.valueOf(username));
-    ResultSet res = ps.executeQuery();
+      UserInfo fetchedUser = new UserInfo();
+      fetchedUser.lastLoggedInTime = -1; // sentinel value
 
-    UserInfo fetchedUser = new UserInfo();
-    fetchedUser.lastLoggedInTime = -1; // sentinel value
+      if (res.next()) {
+        fetchedUser.username = res.getString("username");
+        fetchedUser.email = res.getString("email");
+        fetchedUser.hashedPassword = res.getString("hashed_password");
+        fetchedUser.salt = res.getString("salt");
+        fetchedUser.passwordDbPath = res.getString("pwd_db_path");
+        fetchedUser.lastLoggedInTime = res.getLong("last_logged_in");
+      }
 
-    if (res.next()) {
-      fetchedUser.username = res.getString("username");
-      fetchedUser.email = res.getString("email");
-      fetchedUser.hashedPassword = res.getString("hashed_password");
-      fetchedUser.salt = res.getString("salt");
-      fetchedUser.passwordDbPath = res.getString("pwd_db_path");
-      fetchedUser.lastLoggedInTime = res.getLong("last_logged_in");
+      return fetchedUser;
     }
 
-    return fetchedUser;
   }
 
   public UserInfo getUserInfoByEmail(String email) throws SQLException {
     String query = "SELECT * FROM `" + EnvVars.MASTER_USER_TABLE + "` WHERE `email`=?";
 
-    PreparedStatement ps = con.prepareStatement(query);
+    try (PreparedStatement ps = con.prepareStatement(query)) {
+      ps.setString(1, String.valueOf(email));
+      ResultSet res = ps.executeQuery();
 
-    ps.setString(1, String.valueOf(email));
-    ResultSet res = ps.executeQuery();
+      UserInfo fetchedUser = new UserInfo();
+      fetchedUser.lastLoggedInTime = -1; // sentinel value
 
-    UserInfo fetchedUser = new UserInfo();
-    fetchedUser.lastLoggedInTime = -1; // sentinel value
+      if (res.next()) {
+        fetchedUser.username = res.getString("username");
+        fetchedUser.email = res.getString("email");
+        fetchedUser.hashedPassword = res.getString("hashed_password");
+        fetchedUser.salt = res.getString("salt");
+        fetchedUser.passwordDbPath = res.getString("pwd_db_path");
+        fetchedUser.lastLoggedInTime = res.getLong("last_logged_in");
+      }
 
-    if (res.next()) {
-      fetchedUser.username = res.getString("username");
-      fetchedUser.email = res.getString("email");
-      fetchedUser.hashedPassword = res.getString("hashed_password");
-      fetchedUser.salt = res.getString("salt");
-      fetchedUser.passwordDbPath = res.getString("pwd_db_path");
-      fetchedUser.lastLoggedInTime = res.getLong("last_logged_in");
+      return fetchedUser;
     }
-
-    return fetchedUser;
   }
 
   public boolean updateLastLoginTime(String username, long time) throws SQLException {
     String query = "UPDATE `" + EnvVars.MASTER_USER_TABLE + "` SET `last_logged_in`=? WHERE `username`=?";
 
-    PreparedStatement ps = con.prepareStatement(query);
-    ps.setLong(1, time);
-    ps.setString(2, username);
+    try (PreparedStatement ps = con.prepareStatement(query)) {
+      ps.setLong(1, time);
+      ps.setString(2, username);
 
-    int opStat = ps.executeUpdate();
+      int opStat = ps.executeUpdate();
 
-    if (opStat == 1) {
-      return true;
+      if (opStat == 1) {
+        return true;
+      }
+      return false;
     }
-    return false;
   }
 }
