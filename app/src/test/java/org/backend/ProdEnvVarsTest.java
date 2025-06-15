56ebb0ee12fee0cb62af71d0014665ac7f582582
Twaha Rahman <mahi6703890@gmail.com>
2025-06-15 15:21:00 +0600

    Update the sample.env file and the tests for parsing it

--- app/src/test/java/org/backend/ProdEnvVarsTest.java
+++ app/src/test/java/org/backend/ProdEnvVarsTest.java
@@ -6,12 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProdEnvVarsTest {
   @Test
   void envFileGetsParsedProperly() {
-    ProdEnvVars env = new ProdEnvVars();
+    assertDoesNotThrow(() -> {
+      ProdEnvVars env = new ProdEnvVars();
 
-    assertEquals(env.get("DATABASE_NAME"), "YAPM");
-    assertEquals(env.get("DATABASE_BASE_URL"), "jdbc:mysql://localhost:3306/");
-    assertEquals(env.get("DATABASE_USER"), "root");
-    assertEquals(env.get("DATABASE_PASSWORD"), "");
-    assertEquals(env.get("MASTER_USER_TABLE"), "master_users_table");
+      assert (!env.get("DATABASE_NAME").isEmpty());
+      assert (!env.get("DATABASE_BASE_URL").isEmpty());
+      assert (!env.get("DATABASE_USER").isEmpty());
+      assert (!env.get("MASTER_USER_TABLE").isEmpty());
+    });
   }
 }
