e3d9ae2169502871007cb651552b74633563f39f
Twaha Rahman <mahi6703890@gmail.com>
2025-06-16 22:51:26 +0600

    Fix DB setup code for cloud db

--- app/src/test/java/org/backend/CloudDbConnectionTest.java
+++ app/src/test/java/org/backend/CloudDbConnectionTest.java
@@ -1,11 +1,20 @@
 package org.backend;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Random;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CloudDbConnectionTest {
+  @BeforeAll
+  void setup() {
+    SetupDB.init();
+  }
+
   int getRandomNum() {
     // generate a random number from 1 to 90,000
     final int MIN = 1;
